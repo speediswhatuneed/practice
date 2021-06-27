5f8cd08ce12f9a99b5c228480248e2a0f9a0023b
Akash <mr.akash857@gmail.com>
2021-06-27 19:53:48 +0530

    commiting next

--- src/main/java/com/example/demo/SpringgithubApplication.java
+++ src/main/java/com/example/demo/SpringgithubApplication.java
@@ -8,6 +8,7 @@ public class SpringgithubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringgithubApplication.class, args);
+		System.out.println("inside dev");
 	}
 
 }
